c0a56320339c456156836a604c831635173e2941
macajouu <mgie1936@scs.ubbcluj.ro>
2017-11-24 19:48:03 +0200

    created listview with games

---
+++ app/src/main/java/com/example/maca/rentagame/model/Game.java
@@ -0,0 +1,66 @@
+package com.example.maca.rentagame.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Maca on 11/24/2017.
+ */
+
+public class Game implements Serializable
+{
+    private Integer id;
+    private String name;
+    private String releaseYear;
+    private String producer;
+
+    public Game()
+    {
+        id = 0;
+        name = releaseYear = producer = "";
+    }
+
+    public Game(Integer id, String name, String releaseYear, String producer) {
+        this.id = id;
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.producer = producer;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", releaseYear='" + releaseYear + '\'' +
+                ", producer='" + producer + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/maca/rentagame/utils/GameListAdapter.java
@@ -0,0 +1,92 @@
+package com.example.maca.rentagame.utils;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.maca.rentagame.R;
+import com.example.maca.rentagame.model.Game;
+
+import java.util.List;
+
+/**
+ * Created by Maca on 11/24/2017.
+ */
+
+public class GameListAdapter extends BaseAdapter
+{
+    private Context context;
+    private List<Game> games;
+
+    public GameListAdapter(Context context, List<Game> games)
+    {
+        this.context = context;
+        this.games = games;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return games.size();
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return games.get(position);
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return position;
+    }
+
+    public Game getGameById(Integer id)
+    {
+        for(Game g : games)
+        {
+            if(g.getId() == id)
+            {
+                return g;
+            }
+        }
+        return  games.get(id);
+    }
+
+    public void addGame(Game game)
+    {
+        games.add(game);
+    }
+
+    public void updateGame(Game game)
+    {
+        Game g = getGameById(game.getId());
+
+        g.setName(game.getName());
+        g.setReleaseYear(game.getReleaseYear());
+        g.setProducer(game.getProducer());
+    }
+
+    @Override
+    public View getView(int pos, View convertView, ViewGroup parent)
+    {
+        View view = View.inflate(this.context, R.layout.game, null);
+
+        TextView nameView = (TextView) view.findViewById(R.id.nameTextView);
+        TextView yearView = (TextView) view.findViewById(R.id.yearTextView);
+        TextView producerView = (TextView) view.findViewById(R.id.producerTextView);
+
+        Game currentGame = games.get(pos);
+
+        nameView.setText(currentGame.getName());
+        yearView.setText(currentGame.getReleaseYear());
+        producerView.setText(currentGame.getProducer());
+
+        view.setTag(currentGame);
+
+        return view;
+    }
+}
